73a2bb9ed6ed13cc56719b21ebe0bd1b84245c7e
FireRadical22 <97332558+FireRadical22@users.noreply.github.com>
2022-06-07 10:48:03 +0800

    Small Edits to ability Randomiser

--- Assets/Scripts/EasyAIPaddle.cs
+++ Assets/Scripts/EasyAIPaddle.cs
@@ -7,29 +7,15 @@ public class EasyAIPaddle : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     public Vector3 startPosition;
+    public GameObject ObjectTracking;
 
     // Update is called once per frame
     void Update()
     {
         //AI Movement
-        GameObject ball = GameObject.Find("Ball");
-        float yPaddle = rb.position.y;
-        float distance = ball.transform.position.y - yPaddle;
-        if (distance > 5.0)
-        {
-            while (Mathf.Abs(yPaddle - ball.transform.position.y) > 2.0)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, speed);
-            }
-        } else if (distance < -5.0)
-        {
-            while (Mathf.Abs(yPaddle- ball.transform.position.y) < -2.0)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, -1 * speed);
-            }
-        } else 
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
+        Rigidbody2D paddleBody = gameObject.GetComponent<Rigidbody2D>();
+        if (paddleBody.position.y < ObjectTracking.transform.position.y){
+            paddleBody.velocity = new Vector2()
         }
         
     }
