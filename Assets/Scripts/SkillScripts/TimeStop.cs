b39ae7e2e533518dec5d0b5aaf63a8f19ed9395d
FireRadical22 <97332558+FireRadical22@users.noreply.github.com>
2023-06-21 12:37:43 +0800

    Fix ItemBox jittering
    Fix issue where ItemBox was jittering in singleplayer.

--- Assets/Scripts/SkillScripts/TimeStop.cs
+++ Assets/Scripts/SkillScripts/TimeStop.cs
@@ -8,12 +8,16 @@ public class TimeStop : TimeAbility
 
     public static Vector2 resultantVelocity;
     float playerXPosition;
+    float playerYPosition;
 
     private GameObject ball;
 
     // Ability User is passed in as affectedObject
     public override void Activate(GameObject affectedObject)
     {
+        playerXPosition = affectedObject.GetComponent<Transform>().position.x;
+        playerYPosition = affectedObject.GetComponent<Transform>().position.y;
+
         SoundManager.PlayTimeStopSound();
         ball = affectedObject.GetComponent<AbilityHolder>().ball;
 
@@ -21,7 +25,6 @@ public class TimeStop : TimeAbility
         resultantVelocity = ball.GetComponent<Rigidbody2D>().velocity;
         ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 
-        playerXPosition = affectedObject.GetComponent<Transform>().position.x;
         affectedObject.GetComponent<Paddle>().canMoveDuringTimeStop = true;
         Paddle.isTimeStopped = true;
     }
@@ -35,7 +38,7 @@ public class TimeStop : TimeAbility
 
         Vector3 pos = affectedObject.GetComponent<Transform>().position;
 
-        affectedObject.transform.position = new Vector3(playerXPosition, affectedObject.transform.position.y, 0);
+        affectedObject.transform.position = new Vector3(playerXPosition, playerYPosition, 0);
         affectedObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
 
